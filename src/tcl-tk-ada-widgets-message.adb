4761e668eebcf1437068c0a7d8de56bd0e852955
Bartek thindil Jasicki <thindil@laeran.pl>
2020-01-05 09:41:40 +0100

    moved message widget to separated package

---
+++ src/tcl-tk-ada-widgets-message.adb
@@ -0,0 +1,56 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- Tashy is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+package body Tcl.Tk.Ada.Widgets.Message is
+
+   function Create
+     (pathName: in String; options: in String := "") return Tk_Message is
+   begin --  Create
+      return Create(Context, pathName, options);
+   end Create;
+
+   procedure Create
+     (Widgt: out Tk_Message; pathName: in String; options: in String := "") is
+   begin --  Create
+      Widgt := Create(Context, pathName, options);
+   end Create;
+
+   function Create
+     (Interp: in Tcl_Interp; pathName: in String; options: in String := "")
+      return Tk_Message is
+      --
+      The_Widget: Tk_Message;
+   begin --  Create
+      The_Widget.Interp := Interp;
+      The_Widget.Name := C.Strings.New_String(pathName);
+      Tcl_Eval(The_Widget.Interp, "message " & pathName & " " & options);
+      return The_Widget;
+   end Create;
+
+   procedure Create
+     (Widgt: out Tk_Message; Interp: in Tcl_Interp; pathName: in String;
+      options: in String := "") is
+   begin --  Create
+      Widgt := Create(Interp, pathName, options);
+   end Create;
+
+end Tcl.Tk.Ada.Widgets.Message;
