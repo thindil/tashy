08c15c3e99cc077ae8db16b0e647b68f279dea2f
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-01-10 08:50:28 +0000

    updated code documentation
    FossilOrigin-Name: 9f64370d9073fded2afed7b53351eb9fcae6bdc6e2ef3174fb0915bfd0003323

--- src/tk/tcl-tk-ada-widgets-ttkbutton.ads
+++ src/tk/tcl-tk-ada-widgets-ttkbutton.ads
@@ -27,13 +27,28 @@
 package Tcl.Tk.Ada.Widgets.TtkButton is
 -- ****
 
+   -- ****t* TtkButton/Ttk_Button
+   -- FUNCTION
+   -- This is a non-abstract type derived directly from Tk_Widget.
+   -- Each of the derived widgets redefines the Create subprogram
+   -- in order to create the correct type of widget.
+   -- SOURCE
    type Ttk_Button is new Tk_Widget with private;
+   -- ****
 
    -- ****f* TtkButton/Create
    -- FUNCTION
-   -- Creates a new Ttk_Button in the specified interpreter. If interpreter
-   -- is null, use "contextual" interpreter. Options may be specified via the
-   -- "options" parameter or the option database to configure the widget.
+   -- Creates a new Ttk_Button in the specified interpreter.
+   -- PARAMETERS
+   -- Widgt    - Ttk_Button which will be created
+   -- pathName - Tk path (starts with dot) for the widget
+   -- options  - Options which will be passed to the widget. Default value is
+   --            empty
+   -- Interp   - Tcl interpreter to which the widget will be created. If null,
+   --            the widget will be created in the "contextual" interpreter.
+   --            Default value is null.
+   -- RESULT
+   -- Newly created Ttk_Button
    -- SOURCE
    function Create
      (pathName: in String; options: in String := "";
