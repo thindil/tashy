791c7f81cdadba79fe8715468f0171317e9baff3
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-01-11 06:49:56 +0000

    updated code documentation
    FossilOrigin-Name: eb428d6c96e87ea0f69e307da02d4489f35049b489a3946548f4d56abc72d781

--- src/tk/tcl-tk-ada-widgets-frame.ads
+++ src/tk/tcl-tk-ada-widgets-frame.ads
@@ -45,9 +45,17 @@ package Tcl.Tk.Ada.Widgets.Frame is
 
    -- ****f* Frame/Create
    -- FUNCTION
-   -- Creates a new Tk_Frame in the specified interpreter. If interpreter
-   -- is null, use "contextual" interpreter. Options may be specified via the
-   -- "options" parameter or the option database to configure the widget.
+   -- Creates a new Tk_Frame in the specified interpreter.
+   -- PARAMETERS
+   -- Widgt    - Tk_Frame which will be created
+   -- pathName - Tk path (starts with dot) for the widget
+   -- options  - Options which will be passed to the widget. Default value is
+   --            empty
+   -- Interp   - Tcl interpreter to which the widget will be created. If null,
+   --            the widget will be created in the "contextual" interpreter.
+   --            Default value is null.
+   -- RESULT
+   -- Newly created Tk_Frame
    -- SOURCE
    function Create
      (pathName: in String; options: in String := "";
