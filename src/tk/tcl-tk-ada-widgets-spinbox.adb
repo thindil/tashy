f69b8bef4f02d5d65d2629a610287c840b4a0661
Bartek thindil Jasicki <thindil@laeran.pl>
2020-01-15 07:26:15 +0100

    added binding for spinbox

---
+++ src/tk/tcl-tk-ada-widgets-spinbox.adb
@@ -0,0 +1,47 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+package body Tcl.Tk.Ada.Widgets.SpinBox is
+
+   function Create
+     (pathName: in String; options: in String := "";
+      Interp: in Tcl_Interp := null) return Tk_SpinBox is
+      The_Widget: Tk_SpinBox;
+   begin
+      if Interp = null then
+         The_Widget.Interp := Context;
+      else
+         The_Widget.Interp := Interp;
+      end if;
+      The_Widget.Name := C.Strings.New_String(pathName);
+      Tcl_Eval(The_Widget.Interp, "spinbox " & pathName & " " & options);
+      return The_Widget;
+   end Create;
+
+   procedure Create
+     (Widgt: out Tk_SpinBox; pathName: in String; options: in String := "";
+      Interp: in Tcl_Interp := null) is
+   begin
+      Widgt := Create(pathName, options, Interp);
+   end Create;
+
+end Tcl.Tk.Ada.Widgets.SpinBox;
