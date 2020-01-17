dbd06c90338b02efe0e7db436eab849d17d25c18
Bartek thindil Jasicki <thindil@laeran.pl>
2020-01-17 07:47:13 +0100

    added binding for ttk::scale

---
+++ src/tk/tcl-tk-ada-widgets-ttkscale.ads
@@ -0,0 +1,72 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+-- ****h* Widgets/Scale
+-- FUNCTION
+-- Provides code for manipulate Tk widget Ttk::scale
+-- SOURCE
+package Tcl.Tk.Ada.Widgets.TtkScale is
+-- ****
+
+   -- ****t* Scale/Ttk_Scale
+   -- FUNCTION
+   -- This is a non-abstract type derived directly from Tk_Widget.
+   -- Each of the derived widgets redefines the Create subprogram
+   -- in order to create the correct type of widget.
+   -- SOURCE
+   type Ttk_Scale is new Tk_Widget with private;
+   -- ****
+
+   -- ****f* Scale/Create
+   -- FUNCTION
+   -- Creates a new Ttk_Scale in the specified interpreter.
+   -- PARAMETERS
+   -- Widgt    - Ttk_Scale which will be created
+   -- pathName - Tk path (starts with dot) for the widget
+   -- options  - Options which will be passed to the widget. Default value is
+   --            empty
+   -- Interp   - Tcl interpreter to which the widget will be created. If null,
+   --            the widget will be created in the "contextual" interpreter.
+   --            Default value is null.
+   -- RESULT
+   -- Newly created Ttk_Scale
+   -- SOURCE
+   function Create
+     (pathName: in String; options: in String := "";
+      Interp: in Tcl_Interp := null) return Ttk_Scale;
+   procedure Create
+     (Widgt: out Ttk_Scale; pathName: in String; options: in String := "";
+      Interp: in Tcl_Interp := null);
+   -- ****
+
+private
+
+   type Ttk_Scale is new Tk_Widget with null record;
+
+end Tcl.Tk.Ada.Widgets.TtkScale;
