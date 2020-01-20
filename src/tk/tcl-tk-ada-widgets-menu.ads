220978d46daa185222782f704a771a84d763f82c
Bartek thindil Jasicki <thindil@laeran.pl>
2020-01-20 08:05:00 +0100

    back to old Add command

--- src/tk/tcl-tk-ada-widgets-menu.ads
+++ src/tk/tcl-tk-ada-widgets-menu.ads
@@ -20,8 +20,6 @@
 -- however invalidate any other reasons why the executable file might be
 -- covered by the GNU Public License.
 
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-
 -- ****h* Widgets/Menu
 -- FUNCTION
 -- Provides code for manipulate Tk widget Menu
@@ -36,67 +34,6 @@ package Tcl.Tk.Ada.Widgets.Menu is
    -- in order to create the correct type of widget.
    -- SOURCE
    type Tk_Menu is new Tk_Widget with private;
-   -- ****
-
-   -- ****t* Menu/Menu_Items
-   -- FUNCTION
-   -- Types of menu entries
-   -- SOURCE
-   type Menu_Items is (CASCADE, CHECKBUTTON, COMMAND, RADIOBUTTON, SEPARATOR);
-   -- ****
-
-   -- ****t* Menu/Menu_Entry_States
-   -- FUNCTION
-   -- Possible states of menu entry
-   -- SOURCE
-   type Menu_Entry_States is (NORMAL, ACTIVE, DISABLED);
-   -- ****
-
-   -- ****t* Menu/Menu_Entry_Options
-   -- FUNCTION
-   -- Provide options data structure for creating menu entries
-   -- PARAMETERS
-   -- MType         - Menu_Items type of the menu entry
-   -- Label         - Text to display on the menu entry
-   -- Command       - Tcl command to execute when the menu entry was activated
-   -- Underline     - Number of character (starts from 0) in the menu entry
-   --                 label which will be used as keyboard shortcut. -1 mean
-   --                 no keyboard shortcut.
-   -- Accelerator   - Text to display as keyboard accelerator for the menu
-   --                 entry
-   -- State         - Menu_Entry_States state of the menu entry
-   -- Other         - Other, additional options for the menu entry
-   -- SubMenu       - CASCADE menu entry only - path for submenu which will be
-   --                 added to this menu
-   -- CheckVariable - CHECKBUTTON menu entry only - Tcl global variable in
-   --                 which value of the menu entry will be stored when the
-   --                 menu entry will be activated
-   -- Value         - RADIOBUTTON menu entry only - value for the menu entry
-   --                 which will be set when this menu entry will be activated.
-   --                 If empty, this will be value of parameter Label.
-   -- RadioVariable - RADIOBUTTON menu entry only - Tcl  global variable in
-   --                 which value of the menu entry will be stored when the
-   --                 menu entry will be activated
-   -- SOURCE
-   type Menu_Entry_Options(MType: Menu_Items := COMMAND) is record
-      Label: Unbounded_String;
-      Command: Unbounded_String;
-      Underline: Integer range -1 .. Integer'Last;
-      Accelerator: Unbounded_String;
-      State: Menu_Entry_States;
-      Other: Unbounded_String;
-      case MType is
-         when CASCADE =>
-            SubMenu: Unbounded_String;
-         when CHECKBUTTON =>
-            CheckVariable: Unbounded_String;
-         when RADIOBUTTON =>
-            Value: Unbounded_String;
-            RadioVariable: Unbounded_String;
-         when others =>
-            null;
-      end case;
-   end record;
    -- ****
 
    -- ****f* Menu/Create
@@ -126,9 +63,12 @@ package Tcl.Tk.Ada.Widgets.Menu is
    -- Add element to the menu.
    -- PARAMETERS
    -- MenuWidget - Tk_Menu widget to which the element will be added
-   -- Options    - Menu_Entry_Options options for menu entry
+   -- EntryType  - Tk type of entry to add
+   -- Options    - Tk options for menu entry to add. Default value is empty
    -- SOURCE
-   procedure Add(MenuWidget: in Tk_Menu'Class; Options: in Menu_Entry_Options);
+   procedure Add
+     (MenuWidget: in Tk_Menu'Class; EntryType: in String;
+      Options: in String := "");
    -- ****
 
 private
