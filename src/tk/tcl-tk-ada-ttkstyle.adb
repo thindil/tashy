3dfdd0c2d38eb25001f9d6f810f3a8463763bded
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-01-21 06:57:51 +0000

    added binding for ttk::style command
    FossilOrigin-Name: a94516a3154e5abc0300c90b62c10d2e7e9942a776d2406d229d71f84d16df70

---
+++ src/tk/tcl-tk-ada-ttkstyle.adb
@@ -0,0 +1,44 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+with Tcl.Ada; use Tcl.Ada;
+
+package body Tcl.Tk.Ada.TtkStyle is
+
+   function Theme_Names return String is
+   begin
+      Tcl_Eval(Get_Context, "ttk::style theme names");
+      return Tcl_GetResult(Get_Context);
+   end Theme_Names;
+
+   procedure Theme_Use(ThemeName: String) is
+   begin
+      Tcl_Eval(Get_Context, "ttk::style theme use " & ThemeName);
+   end Theme_Use;
+
+   function Theme_Use return String is
+   begin
+      Tcl_Eval(Get_Context, "ttk::style theme use");
+      return Tcl_GetResult(Get_Context);
+   end Theme_Use;
+
+end Tcl.Tk.Ada.TtkStyle;
---
+++ src/tk/tcl-tk-ada-ttkstyle.ads
@@ -0,0 +1,57 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- Tashy is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+-- ****h* TkAda/TtkStyle
+-- FUNCTION
+-- Provides code for manipulate Tk command ttk::style
+-- SOURCE
+package Tcl.Tk.Ada.TtkStyle is
+-- ****
+
+   -- ****f* Tcl.Tk.Ada.TtkStyle/Theme_Names
+   -- FUNCTION
+   -- Get list of all available Ttk themes
+   -- RESULT
+   -- String with list of all available Ttk themes
+   -- SOURCE
+   function Theme_Names return String;
+   -- ****
+
+   -- ****f* Tcl.Tk.Ada.TtkStyle/Theme_Use
+   -- FUNCTION
+   -- Set new Ttk theme and redraw all widgets
+   -- PARAMETERS
+   -- Name of Ttk theme to set
+   -- SOURCE
+   procedure Theme_Use(ThemeName: String);
+   -- ****
+
+   -- ****f* Tcl.Tk.Ada.TtkStyle/Theme_Use
+   -- FUNCTION
+   -- Get currenlty used Ttk theme
+   -- RESULT
+   -- Name of currently used Ttk theme
+   -- SOURCE
+   function Theme_Use return String;
+   -- ****
+
+end Tcl.Tk.Ada.TtkStyle;
