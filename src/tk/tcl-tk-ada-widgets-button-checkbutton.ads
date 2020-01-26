7110f9add37105d3a445aa333a6fd89cc1a1d7a3
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-01-26 06:15:58 +0000

    added some precondition checks
    FossilOrigin-Name: de5d89b1712ac928c134d3149588daeca3b85e2e16ea605374ada74ec68c09b9

--- src/tk/tcl-tk-ada-widgets-button-checkbutton.ads
+++ src/tk/tcl-tk-ada-widgets-button-checkbutton.ads
@@ -50,18 +50,20 @@ package Tcl.Tk.Ada.Widgets.Button.CheckButton is
    -- SOURCE
    function Create
      (pathName: in String; options: in String := "";
-      Interp: in Tcl_Interp := null) return Tk_CheckButton;
+      Interp: in Tcl_Interp := null) return Tk_CheckButton with
+      Pre => pathName /= "";
    procedure Create
      (Widgt: out Tk_CheckButton; pathName: in String; options: in String := "";
-      Interp: in Tcl_Interp := null);
-   -- ****
+      Interp: in Tcl_Interp := null) with
+      Pre => pathName /= "";
+      -- ****
 
-   -- ****f* CheckButton/Deselect
-   -- FUNCTION
-   -- Deselect the button.
-   -- PARAMETERS
-   -- Buttn - Tk_CheckButton to deselect
-   -- SOURCE
+      -- ****f* CheckButton/Deselect
+      -- FUNCTION
+      -- Deselect the button.
+      -- PARAMETERS
+      -- Buttn - Tk_CheckButton to deselect
+      -- SOURCE
    procedure Deselect(Buttn: in Tk_CheckButton);
    -- ****
 
