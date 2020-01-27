4c72b9f7806a4c87fd758ef5d2f5f6c161d835c4
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-01-27 06:27:14 +0000

    added binding for Tk widget text
    FossilOrigin-Name: bd7774c75f41f1cc19ec987debcc75c925893a001f36787f7641dfff6be726cd

---
+++ src/tk/tcl-tk-ada-widgets-text.ads
@@ -0,0 +1,77 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+-- ****h* Widgets/Text
+-- FUNCTION
+-- Provides code for manipulate Tk widget Text
+-- SOURCE
+package Tcl.Tk.Ada.Widgets.Text is
+-- ****
+
+   -- ****t* Text/Tk_Text
+   -- FUNCTION
+   -- This is a non-abstract type derived directly from Tk_Widget.
+   -- Each of the derived widgets redefines the Create subprogram
+   -- in order to create the correct type of widget.
+   -- SOURCE
+   type Tk_Text is new Tk_Widget with private;
+   -- ****
+
+   -- ****f* Text/Create
+   -- FUNCTION
+   -- Creates a new Tk_Text in the specified interpreter.
+   -- PARAMETERS
+   -- Widgt    - Tk_Text which will be created
+   -- pathName - Tk path (starts with dot) for the widget
+   -- options  - Options which will be passed to the widget. Default value is
+   --            empty
+   -- Interp   - Tcl interpreter to which the widget will be created. If null,
+   --            the widget will be created in the "contextual" interpreter.
+   --            Default value is null.
+   -- RESULT
+   -- Newly created Tk_Text
+   -- SOURCE
+   function Create
+     (pathName: in String; options: in String := "";
+      Interp: in Tcl_Interp := null) return Tk_Text;
+   procedure Create
+     (Widgt: out Tk_Text; pathName: in String; options: in String := "";
+      Interp: in Tcl_Interp := null);
+   -- ****
+
+   -- ****f* Text/Insert
+   -- FUNCTION
+   -- Insert text into text widget at selected index
+   -- PARAMETERS
+   -- TextWidget - Tk_Text to which text will be inserted
+   -- Index      - Tk_Text position on which text will be inserted. Lines
+   --              starts from 1, characters starts from 0
+   -- Text       - Text to insert to Tk_Text widget
+   -- SOURCE
+   procedure Insert(TextWidget: in Tk_Text; Index, Text: in String);
+   -- ****
+
+private
+
+   type Tk_Text is new Tk_Widget with null record;
+
+end Tcl.Tk.Ada.Widgets.Text;
