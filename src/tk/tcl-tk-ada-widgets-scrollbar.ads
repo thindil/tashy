0c561fba1af1e8bab9991a3913a49a8a1986ae32
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-02-06 06:06:47 +0000

    added precondition checks
    FossilOrigin-Name: 9714f8b5900b39ba43e52005a3d1a6206c38532aabd051521bbbb6c59674393e

--- src/tk/tcl-tk-ada-widgets-scrollbar.ads
+++ src/tk/tcl-tk-ada-widgets-scrollbar.ads
@@ -59,10 +59,12 @@ package Tcl.Tk.Ada.Widgets.Scrollbar is
    -- SOURCE
    function Create
      (pathName: in String; options: in String := "";
-      Interp: in Tcl_Interp := null) return Tk_Scrollbar;
+      Interp: in Tcl_Interp := null) return Tk_Scrollbar with
+      Pre => pathName /= "";
    procedure Create
      (Widgt: out Tk_Scrollbar; pathName: in String; options: in String := "";
-      Interp: in Tcl_Interp := null);
+      Interp: in Tcl_Interp := null) with
+      Pre => pathName /= "";
    -- ****
 
 private
