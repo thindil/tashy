0b35c0289b49c3c787abd955dccde1595c6395b2
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-02-06 06:07:40 +0000

    added creating relocatable libraries
    FossilOrigin-Name: 6b8c95d1740990820fcb8c09b1c13ce9f9587fb7075218586410b097d3bf65ad

--- scripts/clean.tcl
+++ scripts/clean.tcl
@@ -2,6 +2,7 @@
 # the next line restarts using tclsh \
 exec tclsh "$0" ${1+"$@"}
 
-exec gprclean -P tashy.gpr
+exec gprclean -P tashy.gpr -XLIBRARY_TYPE=static
+exec gprclean -P tashy.gpr -XLIBRARY_TYPE=relocatable
 file delete tash_options.gpr
 file delete [file join [pwd] src tcl_record_sizes.ads]
