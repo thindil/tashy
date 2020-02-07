7ae673e20240fb046f6bd6b65a1006df002837c6
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-02-07 06:10:01 +0000

    added overriding indicator
    FossilOrigin-Name: a6741b480486e26463c807d1aaeefc2f8848537af06b3bb48c2b0c7565ac664e

--- src/tk/tcl-tk-ada-widgets-button.ads
+++ src/tk/tcl-tk-ada-widgets-button.ads
@@ -50,11 +50,11 @@ package Tcl.Tk.Ada.Widgets.Button is
    -- RESULT
    -- Newly created Tk_Button
    -- SOURCE
-   function Create
+   overriding function Create
      (pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) return Tk_Button with
       Pre => pathName /= "";
-   procedure Create
+   overriding procedure Create
      (Widgt: out Tk_Button; pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) with
       Pre => pathName /= "";
