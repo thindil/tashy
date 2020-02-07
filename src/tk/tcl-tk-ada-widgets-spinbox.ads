3fa262eb411383c62dda9a6704ce215b7dc2ac0f
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-02-07 06:09:45 +0000

    added some precondition checks
    FossilOrigin-Name: 307449393033dfd2edc1007c645a528c2718b8d9e5368aae48467698b0c10c2e

--- src/tk/tcl-tk-ada-widgets-spinbox.ads
+++ src/tk/tcl-tk-ada-widgets-spinbox.ads
@@ -59,10 +59,12 @@ package Tcl.Tk.Ada.Widgets.SpinBox is
    -- SOURCE
    function Create
      (pathName: in String; options: in String := "";
-      Interp: in Tcl_Interp := null) return Tk_SpinBox;
+      Interp: in Tcl_Interp := null) return Tk_SpinBox with
+      Pre => pathName /= "";
    procedure Create
      (Widgt: out Tk_SpinBox; pathName: in String; options: in String := "";
-      Interp: in Tcl_Interp := null);
+      Interp: in Tcl_Interp := null) with
+      Pre => pathName /= "";
    -- ****
 
 private
