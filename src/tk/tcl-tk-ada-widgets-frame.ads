247e7e696c2aacf22cdb5820b16f428e6977597d
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-02-09 06:35:26 +0000

    added overriding indicator
    FossilOrigin-Name: 40ef454c4cdf05a3652a07aa808ce3b94c279d7c2030b6799dd8f88923704b33

--- src/tk/tcl-tk-ada-widgets-frame.ads
+++ src/tk/tcl-tk-ada-widgets-frame.ads
@@ -50,11 +50,11 @@ package Tcl.Tk.Ada.Widgets.Frame is
    -- RESULT
    -- Newly created Tk_Frame
    -- SOURCE
-   function Create
+   overriding function Create
      (pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) return Tk_Frame with
       Pre => pathName /= "";
-   procedure Create
+   overriding procedure Create
      (Widgt: out Tk_Frame; pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) with
       Pre => pathName /= "";
