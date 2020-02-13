3e64b39bbee1d8c1386573c8ce3c054f859604f2
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-02-13 06:24:41 +0000

    added overriding indicator
    FossilOrigin-Name: 295ac8973ca208e55aac1bc01f01c99f063c171d9f08b9bb84c8bda2360352f5

--- src/tk/tcl-tk-ada-widgets-message.ads
+++ src/tk/tcl-tk-ada-widgets-message.ads
@@ -50,11 +50,11 @@ package Tcl.Tk.Ada.Widgets.Message is
    -- RESULT
    -- Newly created Tk_Message
    -- SOURCE
-   function Create
+   overriding function Create
      (pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) return Tk_Message with
       Pre => pathName /= "";
-   procedure Create
+   overriding procedure Create
      (Widgt: out Tk_Message; pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) with
       Pre => pathName /= "";
