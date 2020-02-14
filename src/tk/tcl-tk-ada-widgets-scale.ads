fc1e91e5a25d16b4505dcd06ad4897af4bd2633c
Bartek thindil Jasicki <thindil@laeran.pl>
2020-02-14 09:28:33 +0100

    added overriding indicator

--- src/tk/tcl-tk-ada-widgets-scale.ads
+++ src/tk/tcl-tk-ada-widgets-scale.ads
@@ -57,11 +57,11 @@ package Tcl.Tk.Ada.Widgets.Scale is
    -- RESULT
    -- Newly created Tk_Scale
    -- SOURCE
-   function Create
+   overriding function Create
      (pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) return Tk_Scale with
       Pre => pathName /= "";
-   procedure Create
+   overriding procedure Create
      (Widgt: out Tk_Scale; pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) with
       Pre => pathName /= "";
