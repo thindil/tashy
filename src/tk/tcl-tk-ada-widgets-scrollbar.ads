254d50085e4d144766bcbbf34173ebee8efc00e4
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-02-15 08:37:22 +0000

    added overriding indicator
    FossilOrigin-Name: e7fb95b227b0e79cb625a65d541b7d31ab9da344288a45830258148504047dad

--- src/tk/tcl-tk-ada-widgets-scrollbar.ads
+++ src/tk/tcl-tk-ada-widgets-scrollbar.ads
@@ -57,11 +57,11 @@ package Tcl.Tk.Ada.Widgets.Scrollbar is
    -- RESULT
    -- Newly created Tk_Scrollbar
    -- SOURCE
-   function Create
+   overriding function Create
      (pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) return Tk_Scrollbar with
       Pre => pathName /= "";
-   procedure Create
+   overriding procedure Create
      (Widgt: out Tk_Scrollbar; pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) with
       Pre => pathName /= "";
