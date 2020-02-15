c2668ed0e16d61720adc4c949ee85d2286b1b003
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-02-15 08:37:00 +0000

    added some precondition checks
    FossilOrigin-Name: e45186d31e237e68305fd2cc92e5bab1e43554b900ae6f1df21ae3353520b89e

--- src/tk/tcl-tk-ada-widgets-ttklabel.ads
+++ src/tk/tcl-tk-ada-widgets-ttklabel.ads
@@ -52,11 +52,13 @@ package Tcl.Tk.Ada.Widgets.TtkLabel is
    -- SOURCE
    function Create
      (pathName: in String; options: in String := "";
-      Interp: in Tcl_Interp := null) return Ttk_Label;
+      Interp: in Tcl_Interp := null) return Ttk_Label with
+      Pre => pathName /= "";
    procedure Create
      (Widgt: out Ttk_Label; pathName: in String; options: in String := "";
-      Interp: in Tcl_Interp := null);
-   -- ****
+      Interp: in Tcl_Interp := null) with
+      Pre => pathName /= "";
+      -- ****
 
 private
 
