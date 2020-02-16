fa8ea6217967c017a60a011c280c1c07230ad6ff
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-02-16 09:05:18 +0000

    added some precondition checks
    FossilOrigin-Name: 2efefb64672d06617dba1f8bf75fa83531aeddc3f9704094634a1c75303d915f

--- src/tk/tcl-tk-ada-widgets-ttkmenubutton.ads
+++ src/tk/tcl-tk-ada-widgets-ttkmenubutton.ads
@@ -59,10 +59,12 @@ package Tcl.Tk.Ada.Widgets.TtkMenuButton is
    -- SOURCE
    function Create
      (pathName: in String; options: in String := "";
-      Interp: in Tcl_Interp := null) return Ttk_MenuButton;
+      Interp: in Tcl_Interp := null) return Ttk_MenuButton with
+      Pre => pathName /= "";
    procedure Create
      (Widgt: out Ttk_MenuButton; pathName: in String; options: in String := "";
-      Interp: in Tcl_Interp := null);
+      Interp: in Tcl_Interp := null) with
+      Pre => pathName /= "";
    -- ****
 
 private
