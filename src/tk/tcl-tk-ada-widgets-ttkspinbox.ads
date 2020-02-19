23a3943d2a87112e51872373c7915b1cb264eeba
Bartek thindil Jasicki <thindil@laeran.pl>
2020-02-19 07:07:05 +0100

    added some precondition checks

--- src/tk/tcl-tk-ada-widgets-ttkspinbox.ads
+++ src/tk/tcl-tk-ada-widgets-ttkspinbox.ads
@@ -59,11 +59,13 @@ package Tcl.Tk.Ada.Widgets.TtkSpinBox is
    -- SOURCE
    function Create
      (pathName: in String; options: in String := "";
-      Interp: in Tcl_Interp := null) return Ttk_SpinBox;
+      Interp: in Tcl_Interp := null) return Ttk_SpinBox with
+      Pre => pathName /= "";
    procedure Create
      (Widgt: out Ttk_SpinBox; pathName: in String; options: in String := "";
-      Interp: in Tcl_Interp := null);
-   -- ****
+      Interp: in Tcl_Interp := null) with
+      Pre => pathName /= "";
+      -- ****
 
 private
 
