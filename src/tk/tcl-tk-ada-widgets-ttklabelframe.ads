00963e62621850805c50b2ce496b89d3d58c6eda
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-02-20 06:16:28 +0000

    added overriding indicator
    FossilOrigin-Name: d85d8bf7926baf58398b6bc17347d19db2d81ddbb18a19b50592b3caa7c1285f

--- src/tk/tcl-tk-ada-widgets-ttklabelframe.ads
+++ src/tk/tcl-tk-ada-widgets-ttklabelframe.ads
@@ -57,11 +57,11 @@ package Tcl.Tk.Ada.Widgets.TtkLabelFrame is
    -- RESULT
    -- Newly created Ttk_LabelFrame
    -- SOURCE
-   function Create
+   overriding function Create
      (pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) return Ttk_LabelFrame with
       Pre => pathName /= "";
-   procedure Create
+   overriding procedure Create
      (Widgt: out Ttk_LabelFrame; pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) with
       Pre => pathName /= "";
