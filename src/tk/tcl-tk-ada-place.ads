2a4c3aa9ec85a0b478dada29fe4d56696647d487
Bartek thindil Jasicki <thindil@laeran.pl>
2020-04-13 07:01:54 +0200

    added binding for command place

---
+++ src/tk/tcl-tk-ada-place.ads
@@ -0,0 +1,107 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- Tashy is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+with Tcl.Tk.Ada.Widgets; use Tcl.Tk.Ada.Widgets;
+
+-- ****h* TkAda/TkPlace
+-- FUNCTION
+-- Provides code for manipulate Tk command Place
+-- SOURCE
+package Tcl.Tk.Ada.Place is
+-- ****
+
+   -- ****f* TkPlace/Place
+   -- FUNCTION
+   -- Place selected widget with selected options
+   -- PARAMETERS
+   -- Slave   - Tk_Widget to place
+   -- Options - Option to place action
+   -- HISTORY
+   -- 8.6.4 - Added
+   -- TODO
+   -- Replace it with higher level of binding
+   -- SOURCE
+   procedure Place(Slave: in Tk_Widget'Class; Options: in String := "");
+   -- ****
+
+   -- ****f* TkPlace/Place_Configure
+   -- FUNCTION
+   -- Tells the packer how to configure the specified Slave window.
+   -- PARAMETERS
+   -- Slave   - Tk_Widget to configure
+   -- Options - Place options for the slave
+   -- HISTORY
+   -- 8.6.4 - Added
+   -- TODO
+   -- Replace it with higher level of binding
+   -- SOURCE
+   procedure Place_Configure
+     (Slave: in Tk_Widget'Class; Options: in String := "");
+   -- ****
+
+   -- ****f* TkPlace/Place_Forget
+   -- FUNCTION
+   -- Removes the Slave window from the packing list for its master
+   -- and unmaps their windows.
+   -- PARAMETERS
+   -- Slave - Tk_Widget to remove
+   -- HISTORY
+   -- 8.6.4 - Added
+   -- TODO
+   -- Replace it with higher level of binding
+   -- SOURCE
+   procedure Place_Forget(Slave: in Tk_Widget'Class);
+   -- ****
+
+   -- ****f* TkPlace/Place_Info
+   -- FUNCTION
+   -- Get place options of the selected widget
+   -- PARAMETERS
+   -- Slave - Tk_Widget to query
+   -- RESULT
+   -- String with list whose elements are the current configuration
+   -- state of the specified Slave window.
+   -- HISTORY
+   -- 8.6.4 - Added
+   -- TODO
+   -- Replace it with higher level of binding
+   -- SOURCE
+   function Place_Info(Slave: in Tk_Widget'Class) return String;
+   -- ****
+
+   -- ****f* TkPlace/Place_Slaves
+   -- FUNCTION
+   -- Get list of slaves widget of selected widget
+   -- PARAMETERS
+   -- Master - Tk_Widget which will be queried for slaves
+   -- RESULT
+   -- Returns a list of slaves in the packing order of the specified
+   -- Master window.
+   -- HISTORY
+   -- 8.6.4 - Added
+   -- TODO
+   -- Replace it with higher level of binding
+   -- SOURCE
+   function Place_Slaves(Master: in Tk_Widget'Class) return String;
+   -- ****
+
+end Tcl.Tk.Ada.Place;
