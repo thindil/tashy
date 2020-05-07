801f75feab4b95f85dc50c5be1aa13a115a68397
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-05-07 04:56:21 +0000

    updated code documenation
    FossilOrigin-Name: 2d15df4b5fc6896149847ea82b7b0ab659d6c25abe3d98f096c412cc92820b4e

--- src/tk/tcl-tk-ada-widgets-ttkframe.ads
+++ src/tk/tcl-tk-ada-widgets-ttkframe.ads
@@ -43,11 +43,10 @@ package Tcl.Tk.Ada.Widgets.TtkFrame is
    type Ttk_Frame is new Tk_Widget with private;
    -- ****
 
-   -- ****f* TtkFrame/Create
+   -- ****f* TtkFrame/Create (function)
    -- FUNCTION
    -- Creates a new Ttk_Frame in the specified interpreter.
    -- PARAMETERS
-   -- Widgt    - Ttk_Frame which will be created
    -- pathName - Tk path (starts with dot) for the widget
    -- options  - Options which will be passed to the widget. Default value is
    --            empty
@@ -61,6 +60,20 @@ package Tcl.Tk.Ada.Widgets.TtkFrame is
      (pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) return Ttk_Frame with
       Pre => pathName /= "";
+      -- ****
+
+   -- ****f* TtkFrame/Create (procedure)
+   -- FUNCTION
+   -- Creates a new Ttk_Frame in the specified interpreter.
+   -- PARAMETERS
+   -- Widgt    - Ttk_Frame which will be created
+   -- pathName - Tk path (starts with dot) for the widget
+   -- options  - Options which will be passed to the widget. Default value is
+      --            empty
+   -- Interp   - Tcl interpreter to which the widget will be created. If null,
+      --            the widget will be created in the "contextual" interpreter.
+      --            Default value is null.
+      -- SOURCE
    overriding procedure Create
      (Widgt: out Ttk_Frame; pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) with
