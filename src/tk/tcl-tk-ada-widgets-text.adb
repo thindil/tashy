3011db63fe407f87b8505cb9a61504cc02d20c73
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-05-10 04:49:32 +0000

    added binding for command edit canredo
    FossilOrigin-Name: 76818a865fd4282c1ad6c6a5c18c024484d8940f4566a323881853aa46c8ec30

--- src/tk/tcl-tk-ada-widgets-text.adb
+++ src/tk/tcl-tk-ada-widgets-text.adb
@@ -103,6 +103,12 @@ package body Tcl.Tk.Ada.Widgets.Text is
       return Tcl.Ada.Tcl_GetResult(TextWidget.Interp);
    end Dump;
 
+   function Edit_CanRedo(TextWidget: in Tk_Text) return String is
+   begin
+      Execute_Widget_Command(TextWidget, "edit canredo");
+      return Tcl.Ada.Tcl_GetResult(TextWidget.Interp);
+   end Edit_CanRedo;
+
    procedure Insert(TextWidget: in Tk_Text; Index, Text: in String) is
    begin
       Execute_Widget_Command(TextWidget, "insert", Index & " " & Text);
