d0c167ba64a26c0a1bebc38f855451197a5e7773
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-05-21 07:32:06 +0000

    added binding for command sync
    FossilOrigin-Name: 7c9fb44735105a260242f5ed03e675c2e10f36f0c222748168abb82c803d8c9b

--- src/tk/tcl-tk-ada-widgets-text.adb
+++ src/tk/tcl-tk-ada-widgets-text.adb
@@ -1,24 +1,23 @@
 -- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
 --
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
+-- This program is free software: you can redistribute it and/or modify it
+-- under the terms of the GNU General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option)
+-- any later version.
 --
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+-- FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+-- for more details.
 --
--- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-- You should have received a copy of the GNU General Public License along with
+-- this program. If not, see <http://www.gnu.org/licenses/>.
 --
--- As a special exception, if other files instantiate generics from this
--- unit, or you link this unit with other files to produce an executable,
--- this unit does not by itself cause the resulting executable to be
--- covered by the GNU General Public License. This exception does not
--- however invalidate any other reasons why the executable file might be
--- covered by the GNU Public License.
+-- As a special exception, if other files instantiate generics from this unit,
+-- or you link this unit with other files to produce an executable, this unit
+-- does not by itself cause the resulting executable to be covered by the GNU
+-- General Public License. This exception does not however invalidate any other
+-- reasons why the executable file might be covered by the GNU Public License.
 
 with Tcl.Ada;
 
@@ -297,6 +296,11 @@ package body Tcl.Tk.Ada.Widgets.Text is
    begin
       Execute_Widget_Command(TextWidget, "see", Index);
    end See;
+
+   procedure Sync(TextWidget: in Tk_Text) is
+   begin
+      Execute_Widget_Command(TextWidget, "sync");
+   end Sync;
 
    procedure Tag_Add
      (TextWidget: in Tk_Text; TagName, StartIndex: in String;
