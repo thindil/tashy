0105484129df5ea6389a0f3ecace1cdb696a21d0
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-05-29 07:31:34 +0000

    updated code documentation
    FossilOrigin-Name: c8a979299fc5469c1019aebbf41889e2d4c44e06915db9775e399c1675c2cfcb

--- src/tk/tcl-tk-ada-widgets-button.ads
+++ src/tk/tcl-tk-ada-widgets-button.ads
@@ -48,6 +48,10 @@ package Tcl.Tk.Ada.Widgets.Button is
    --            Default value is null.
    -- RESULT
    -- Newly created Tk_Button
+   -- HISTORY
+   -- 8.6.1 - Moved from Tcl.Tk.Ada.Widgets
+   -- TODO
+   -- Replace it with higher level of binding
    -- SOURCE
    overriding function Create
      (pathName: in String; options: in String := "";
@@ -55,30 +59,38 @@ package Tcl.Tk.Ada.Widgets.Button is
       Pre => pathName /= "";
       -- ****
 
-   -- ****f* Button/Create (procedure)
-   -- FUNCTION
-   -- Creates a new Tk_Button in the specified interpreter.
-   -- PARAMETERS
-   -- Widgt    - Tk_Button which will be created
-   -- pathName - Tk path (starts with dot) for the widget
-   -- options  - Options which will be passed to the widget. Default value is
-      --            empty
-   -- Interp   - Tcl interpreter to which the widget will be created. If null,
-      --            the widget will be created in the "contextual" interpreter.
-      --            Default value is null.
-      -- SOURCE
+     -- ****f* Button/Create (procedure)
+     -- FUNCTION
+     -- Creates a new Tk_Button in the specified interpreter.
+     -- PARAMETERS
+     -- Widgt    - Tk_Button which will be created
+     -- pathName - Tk path (starts with dot) for the widget
+     -- options  - Options which will be passed to the widget. Default value is
+     --            empty
+     -- Interp   - Tcl interpreter to which the widget will be created. If null,
+     --            the widget will be created in the "contextual" interpreter.
+     --            Default value is null.
+     -- HISTORY
+     -- 8.6.1 - Moved from Tcl.Tk.Ada.Widgets
+     -- TODO
+     -- Replace it with higher level of binding
+     -- SOURCE
    overriding procedure Create
      (Widgt: out Tk_Button; pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) with
       Pre => pathName /= "";
-      -- ****
+     -- ****
 
-      -- ****f* Button/Flash
-      -- FUNCTION
-      -- Flash the button.
-      -- PARAMETERS
-      -- Buttn - Tk_Button to flash
-      -- SOURCE
+     -- ****f* Button/Flash
+     -- FUNCTION
+     -- Flash the button.
+     -- PARAMETERS
+     -- Buttn - Tk_Button to flash
+     -- HISTORY
+     -- 8.6.1 - Moved from Tcl.Tk.Ada.Widgets
+     -- TODO
+     -- Replace it with higher level of binding
+     -- SOURCE
    procedure Flash(Buttn: in Tk_Button);
    -- ****
 
@@ -90,10 +102,14 @@ package Tcl.Tk.Ada.Widgets.Button is
    -- options - Tk options for the Tcl command invoke
    -- RESULT
    -- Value returned by the Tcl command associated with the button
+   -- HISTORY
+   -- 8.6.1 - Moved from Tcl.Tk.Ada.Widgets
+   -- TODO
+   -- Replace it with higher level of binding
    -- SOURCE
    function Invoke
      (Buttn: in Tk_Button; options: in String := "") return String;
-   -- ****
+     -- ****
 
 private
 
