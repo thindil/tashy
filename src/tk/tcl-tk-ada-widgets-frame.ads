2bb0961df99d0cb6749f69232a79f15625f81f27
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-05-31 07:26:20 +0000

    updated code documentation
    FossilOrigin-Name: 9c04e2ef2e7ab59a106e558f6a1ae53de4f26ff0812976171979df6a9fec0896

--- src/tk/tcl-tk-ada-widgets-frame.ads
+++ src/tk/tcl-tk-ada-widgets-frame.ads
@@ -48,25 +48,33 @@ package Tcl.Tk.Ada.Widgets.Frame is
    --            Default value is null.
    -- RESULT
    -- Newly created Tk_Frame
+   -- HISTORY
+   -- 8.6.1 - Moved from Tcl.Tk.Ada.Widgets
+   -- TODO
+   -- Replace it with higher level of binding
    -- SOURCE
    overriding function Create
      (pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) return Tk_Frame with
       Pre => pathName /= "";
-      -- ****
+     -- ****
 
-   -- ****f* Frame/Create (procedure)
-   -- FUNCTION
-   -- Creates a new Tk_Frame in the specified interpreter.
-   -- PARAMETERS
-   -- Widgt    - Tk_Frame which will be created
-   -- pathName - Tk path (starts with dot) for the widget
-   -- options  - Options which will be passed to the widget. Default value is
-      --            empty
-   -- Interp   - Tcl interpreter to which the widget will be created. If null,
-      --            the widget will be created in the "contextual" interpreter.
-      --            Default value is null.
-      -- SOURCE
+     -- ****f* Frame/Create (procedure)
+     -- FUNCTION
+     -- Creates a new Tk_Frame in the specified interpreter.
+     -- PARAMETERS
+     -- Widgt    - Tk_Frame which will be created
+     -- pathName - Tk path (starts with dot) for the widget
+     -- options  - Options which will be passed to the widget. Default value is
+     --            empty
+     -- Interp   - Tcl interpreter to which the widget will be created. If null,
+     --            the widget will be created in the "contextual" interpreter.
+     --            Default value is null.
+     -- HISTORY
+     -- 8.6.1 - Moved from Tcl.Tk.Ada.Widgets
+     -- TODO
+     -- Replace it with higher level of binding
+     -- SOURCE
    overriding procedure Create
      (Widgt: out Tk_Frame; pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) with
