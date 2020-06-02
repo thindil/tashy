eb483012d104032491a3a3ea7ab3f75dd4090477
Bartek thindil Jasicki <thindil@laeran.pl>
2020-06-02 09:29:15 +0200

    updated code documentation

--- src/tk/tcl-tk-ada-widgets-listbox.ads
+++ src/tk/tcl-tk-ada-widgets-listbox.ads
@@ -48,25 +48,33 @@ package Tcl.Tk.Ada.Widgets.ListBox is
    --            Default value is null.
    -- RESULT
    -- Newly created Tk_ListBox
+   -- HISTORY
+   -- 8.6.2 - Added
+   -- TODO
+   -- Replace it with higher level of binding
    -- SOURCE
    overriding function Create
      (pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) return Tk_ListBox with
       Pre => pathName /= "";
-      -- ****
+     -- ****
 
-   -- ****f* ListBox/Create (procedure)
-   -- FUNCTION
-   -- Creates a new Tk_ListBox in the specified interpreter.
-   -- PARAMETERS
-   -- Widgt    - Tk_ListBox which will be created
-   -- pathName - Tk path (starts with dot) for the widget
-   -- options  - Options which will be passed to the widget. Default value is
-      --            empty
-   -- Interp   - Tcl interpreter to which the widget will be created. If null,
-      --            the widget will be created in the "contextual" interpreter.
-      --            Default value is null.
-      -- SOURCE
+     -- ****f* ListBox/Create (procedure)
+     -- FUNCTION
+     -- Creates a new Tk_ListBox in the specified interpreter.
+     -- PARAMETERS
+     -- Widgt    - Tk_ListBox which will be created
+     -- pathName - Tk path (starts with dot) for the widget
+     -- options  - Options which will be passed to the widget. Default value is
+     --            empty
+     -- Interp   - Tcl interpreter to which the widget will be created. If null,
+     --            the widget will be created in the "contextual" interpreter.
+     --            Default value is null.
+     -- HISTORY
+     -- 8.6.2 - Added
+     -- TODO
+     -- Replace it with higher level of binding
+     -- SOURCE
    overriding procedure Create
      (Widgt: out Tk_ListBox; pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) with
