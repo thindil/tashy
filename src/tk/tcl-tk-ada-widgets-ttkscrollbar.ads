a6d98d609f5a56e4188c662566eb7dd5a2f04ab8
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-06-05 07:43:12 +0000

    added binding for commands delta, fraction, get and set
    FossilOrigin-Name: 1e42226e59fa35ae8c2aa2c3a0ad7031000857dc11d4d59b22591b8f4c80e645

--- src/tk/tcl-tk-ada-widgets-ttkscrollbar.ads
+++ src/tk/tcl-tk-ada-widgets-ttkscrollbar.ads
@@ -80,6 +80,87 @@ package Tcl.Tk.Ada.Widgets.TtkScrollbar is
       Pre => pathName /= "";
       -- ****
 
+   -- ****f* TtkScrollbar/Scrollbar_Delta
+   -- FUNCTION
+   -- Get the faction change between current Ttk_Scrollbar setting and the
+   -- selected value
+   -- PARAMETERS
+   -- ScrollbarWidget - Ttk_Scrollbar which will be queried
+   -- DeltaX          - An value to check for horizontal scrollbar. Ignored
+   --                   when ScrollbarWidget is a vertical scrollbar.
+   -- DeltaY          - An value to check for vertical scrollbar. Ignored when
+   --                   ScrollbarWidget is a horizontal widget.
+   -- RESULT
+   -- A string with a real number indicating the fractional change in the
+   -- scrollbar setting that corresponds to a given change in slider position.
+   -- HISTORY
+   -- 8.6.6 - Added
+   -- TODO
+   -- Replace it with higher level of binding
+   -- SOURCE
+   function Scrollbar_Delta
+     (ScrollbarWidget: in Ttk_Scrollbar; DeltaX, DeltaY: in String)
+      return String with
+      Pre => DeltaX /= "" and DeltaY /= "";
+      -- ****
+
+      -- ****f* TtkScrollbar/Fraction
+      -- FUNCTION
+      -- Get the fraction of the selected Ttk_Scrollbar at the selected pixel
+      -- PARAMETERS
+      -- ScrollbarWidget - Ttk_Scrollbar which will be queried for the fraction
+      -- X               - X coordinate of the pixel inside ScrollbarWidget
+      -- Y               - Y coordinate of the pixel inside ScrollbarWidget
+      -- RESULT
+      -- Value between 0 and 1 where 0 is top or left of the trough and 1 is
+      -- bottom or right, middle is 0.5.
+      -- HISTORY
+      -- 8.6.6 - Added
+      -- TODO
+      -- Replace it with higher level of binding
+      -- SOURCE
+   function Fraction
+     (ScrollbarWidget: in Ttk_Scrollbar; X, Y: in String) return String with
+      Pre => X /= "" and Y /= "";
+      -- ****
+
+      -- ****f* TtkScrollbar/Get
+      -- FUNCTION
+      -- Get the Ttk_Scrollbar setting for which part of associated widget is
+      -- currently visible
+      -- PARAMETERS
+      -- ScrollbarWidget - Ttk_Scrollbar which will be queried for setting
+      -- RESULT
+      -- Two values of real fraction between 0 and 1 for visible part of the
+      -- associated widget.
+      -- HISTORY
+      -- 8.6.6 - Added
+      -- TODO
+      -- Replace it with higher level of binding
+      -- SOURCE
+   function Get(ScrollbarWidget: in Ttk_Scrollbar) return String;
+   -- ****
+
+      -- ****f* TtkScrollbar/Set
+      -- FUNCTION
+      -- Set the Ttk_Scrollbar setting for which part of associated widget is
+      -- currently visible
+      -- PARAMETERS
+      -- ScrollbarWidget - Ttk_Scrollbar which setting will be changed
+      -- First           - Start fraction of visible part of the associated
+      --                   widget
+      -- Last            - End fraction of visible part of the associated
+      --                   widget
+      -- HISTORY
+      -- 8.6.6 - Added
+      -- TODO
+      -- Replace it with higher level of binding
+      -- SOURCE
+   procedure Set
+     (ScrollbarWidget: in Ttk_Scrollbar; First, Last: in String) with
+      Pre => First /= "" and Last /= "";
+      -- ****
+
 private
 
    type Ttk_Scrollbar is new Tk_Widget with null record;
