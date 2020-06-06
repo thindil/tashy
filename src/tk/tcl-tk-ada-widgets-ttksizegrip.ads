b62873f91e6fa62139e2ac07a3b0881132ecb28d
Bartek thindil Jasicki <thindil@laeran.pl>
2020-06-06 09:27:05 +0200

    added binding for sizegrip widget

---
+++ src/tk/tcl-tk-ada-widgets-ttksizegrip.ads
@@ -0,0 +1,95 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+-- ****h* Widgets/TtkFrame
+-- FUNCTION
+-- Provides code for manipulate Tk widget Ttk::SizeGrip
+-- SOURCE
+package Tcl.Tk.Ada.Widgets.TtkSizeGrip is
+-- ****
+
+   -- ****t* TtkFrame/Ttk_SizeGrip
+   -- FUNCTION
+   -- This is a non-abstract type derived directly from Tk_Widget.
+   -- Each of the derived widgets redefines the Create subprogram
+   -- in order to create the correct type of widget.
+   -- SOURCE
+   type Ttk_SizeGrip is new Tk_Widget with private;
+   -- ****
+
+   -- ****f* TtkFrame/Create (function)
+   -- FUNCTION
+   -- Creates a new Ttk_SizeGrip in the specified interpreter.
+   -- PARAMETERS
+   -- pathName - Tk path (starts with dot) for the widget
+   -- options  - Options which will be passed to the widget. Default value is
+   --            empty
+   -- Interp   - Tcl interpreter to which the widget will be created. If null,
+   --            the widget will be created in the "contextual" interpreter.
+   --            Default value is null.
+   -- RESULT
+   -- Newly created Ttk_SizeGrip
+   -- HISTORY
+   -- 8.6.6 - Added
+   -- TODO
+   -- Replace it with higher level of binding
+   -- SOURCE
+   overriding function Create
+     (pathName: in String; options: in String := "";
+      Interp: in Tcl_Interp := null) return Ttk_SizeGrip with
+      Pre => pathName /= "";
+     -- ****
+
+     -- ****f* TtkFrame/Create (procedure)
+     -- FUNCTION
+     -- Creates a new Ttk_SizeGrip in the specified interpreter.
+     -- PARAMETERS
+     -- Widgt    - Ttk_SizeGrip which will be created
+     -- pathName - Tk path (starts with dot) for the widget
+     -- options  - Options which will be passed to the widget. Default value is
+     --            empty
+     -- Interp   - Tcl interpreter to which the widget will be created. If null,
+     --            the widget will be created in the "contextual" interpreter.
+     --            Default value is null.
+     -- HISTORY
+     -- 8.6.6 - Added
+     -- TODO
+     -- Replace it with higher level of binding
+     -- SOURCE
+   overriding procedure Create
+     (Widgt: out Ttk_SizeGrip; pathName: in String; options: in String := "";
+      Interp: in Tcl_Interp := null) with
+      Pre => pathName /= "";
+      -- ****
+
+private
+
+   type Ttk_SizeGrip is new Tk_Widget with null record;
+
+end Tcl.Tk.Ada.Widgets.TtkSizeGrip;
