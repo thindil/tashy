ef4288985a5e48cfc92e31800355a323a5f4c0a4
Bartek thindil Jasicki <thindil@laeran.pl>
2020-06-10 09:29:05 +0200

    updated code documentation

--- src/tk/tcl-tk-ada-widgets-tentry-spinbox.ads
+++ src/tk/tcl-tk-ada-widgets-tentry-spinbox.ads
@@ -55,25 +55,33 @@ package Tcl.Tk.Ada.Widgets.Tentry.SpinBox is
    --            Default value is null.
    -- RESULT
    -- Newly created Tk_SpinBox
+   -- HISTORY
+   -- 8.6.1 - Added
+   -- TODO
+   -- Replace it with higher level of binding
    -- SOURCE
    overriding function Create
      (pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) return Tk_SpinBox with
       Pre => pathName /= "";
-      -- ****
+     -- ****
 
-   -- ****f* SpinBox/Create (procedure)
-   -- FUNCTION
-   -- Creates a new Tk_SpinBox in the specified interpreter.
-   -- PARAMETERS
-   -- Widgt    - Tk_SpinBox which will be created
-   -- pathName - Tk path (starts with dot) for the widget
-   -- options  - Options which will be passed to the widget. Default value is
-      --            empty
-   -- Interp   - Tcl interpreter to which the widget will be created. If null,
-      --            the widget will be created in the "contextual" interpreter.
-      --            Default value is null.
-      -- SOURCE
+     -- ****f* SpinBox/Create (procedure)
+     -- FUNCTION
+     -- Creates a new Tk_SpinBox in the specified interpreter.
+     -- PARAMETERS
+     -- Widgt    - Tk_SpinBox which will be created
+     -- pathName - Tk path (starts with dot) for the widget
+     -- options  - Options which will be passed to the widget. Default value is
+     --            empty
+     -- Interp   - Tcl interpreter to which the widget will be created. If null,
+     --            the widget will be created in the "contextual" interpreter.
+     --            Default value is null.
+     -- HISTORY
+     -- 8.6.1 - Added
+     -- TODO
+     -- Replace it with higher level of binding
+     -- SOURCE
    overriding procedure Create
      (Widgt: out Tk_SpinBox; pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) with
