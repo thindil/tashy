c8db25b9cd54d56182ab446a17b038f3ad41d5dc
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-06-11 05:34:27 +0000

    updated code documentation
    FossilOrigin-Name: 06383fac29af745acb52b2d2166fdc7d231424aa6e83e974ea61f0c5ea9f50fd

--- src/tk/tcl-tk-ada-widgets-tentry.ads
+++ src/tk/tcl-tk-ada-widgets-tentry.ads
@@ -47,24 +47,32 @@ package Tcl.Tk.Ada.Widgets.TEntry is
    --            Default value is null.
    -- RESULT
    -- Newly created Tk_Entry
+   -- HISTORY
+   -- 8.6.1 - Moved from Tcl.Tk.Ada.Widgets
+   -- TODO
+   -- Replace it with higher level of binding
    -- SOURCE
    overriding function Create
      (pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) return Tk_Entry with
       Pre => pathName /= "";
-      -- ****
+     -- ****
 
-   -- ****f* TEntry/Create (procedure)
-   -- Creates a new Tk_Entry in the specified interpreter.
-   -- PARAMETERS
-   -- Widgt    - Tk_Entry which will be created
-   -- pathName - Tk path (starts with dot) for the widget
-   -- options  - Options which will be passed to the widget. Default value is
-      --            empty
-   -- Interp   - Tcl interpreter to which the widget will be created. If null,
-      --            the widget will be created in the "contextual" interpreter.
-      --            Default value is null.
-      -- SOURCE
+     -- ****f* TEntry/Create (procedure)
+     -- Creates a new Tk_Entry in the specified interpreter.
+     -- PARAMETERS
+     -- Widgt    - Tk_Entry which will be created
+     -- pathName - Tk path (starts with dot) for the widget
+     -- options  - Options which will be passed to the widget. Default value is
+     --            empty
+     -- Interp   - Tcl interpreter to which the widget will be created. If null,
+     --            the widget will be created in the "contextual" interpreter.
+     --            Default value is null.
+     -- HISTORY
+     -- 8.6.1 - Moved from Tcl.Tk.Ada.Widgets
+     -- TODO
+     -- Replace it with higher level of binding
+     -- SOURCE
    overriding procedure Create
      (Widgt: out Tk_Entry; pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) with
