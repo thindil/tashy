40f876c0d4a1691ce6ac833eb24ff23a61e1405c
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-06-14 05:31:25 +0000

    updated code documentation
    FossilOrigin-Name: 882e423b3457ede7738107f3e11c14efc020c57f2f0b5e59ec89dcde6c8da065

--- src/tk/tcl-tk-ada-widgets-toplevel.ads
+++ src/tk/tcl-tk-ada-widgets-toplevel.ads
@@ -41,25 +41,33 @@ package Tcl.Tk.Ada.Widgets.Toplevel is
    --            Default value is null.
    -- RESULT
    -- Newly created Tk_Toplevel
+   -- HISTORY
+   -- 8.6.1 - Moved from Tcl.Tk.Ada.Widgets
+   -- TODO
+   -- Replace it with higher level of binding
    -- SOURCE
    overriding function Create
      (pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) return Tk_Toplevel with
       Pre => pathName /= "";
-      -- ****
+     -- ****
 
-   -- ****f* Toplevel/Create (procedure)
-   -- FUNCTION
-   -- Creates a new Tk_Toplevel in the specified interpreter.
-   -- PARAMETERS
-   -- Widgt    - Tk_Toplevel which will be created
-   -- pathName - Tk path (starts with dot) for the widget
-   -- options  - Options which will be passed to the widget. Default value is
-      --            empty
-   -- Interp   - Tcl interpreter to which the widget will be created. If null,
-      --            the widget will be created in the "contextual" interpreter.
-      --            Default value is null.
-      -- SOURCE
+     -- ****f* Toplevel/Create (procedure)
+     -- FUNCTION
+     -- Creates a new Tk_Toplevel in the specified interpreter.
+     -- PARAMETERS
+     -- Widgt    - Tk_Toplevel which will be created
+     -- pathName - Tk path (starts with dot) for the widget
+     -- options  - Options which will be passed to the widget. Default value is
+     --            empty
+     -- Interp   - Tcl interpreter to which the widget will be created. If null,
+     --            the widget will be created in the "contextual" interpreter.
+     --            Default value is null.
+     -- HISTORY
+     -- 8.6.1 - Moved from Tcl.Tk.Ada.Widgets
+     -- TODO
+     -- Replace it with higher level of binding
+     -- SOURCE
    overriding procedure Create
      (Widgt: out Tk_Toplevel; pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) with
