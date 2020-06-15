023291bef1585cfa7e4ab576cfc3d8b1aba22902
Bartek thindil Jasicki <thindil@laeran.pl>
2020-06-15 07:27:07 +0200

    updated code documentation

--- src/tk/tcl-tk-ada-widgets-ttkbutton-ttkcheckbutton.ads
+++ src/tk/tcl-tk-ada-widgets-ttkbutton-ttkcheckbutton.ads
@@ -48,25 +48,33 @@ package Tcl.Tk.Ada.Widgets.TtkButton.TtkCheckButton is
    --            Default value is null.
    -- RESULT
    -- Newly created Ttk_CheckButton
+   -- HISTORY
+   -- 8.6.1 - Added
+   -- TODO
+   -- Replace it with higher level of binding
    -- SOURCE
    overriding function Create
      (pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) return Ttk_CheckButton with
       Pre => pathName /= "";
-      -- ****
+     -- ****
 
-   -- ****f* TtkCheckButton/Create (procedure)
-   -- FUNCTION
-   -- Creates a new Ttk_CheckButton in the specified interpreter.
-   -- PARAMETERS
-   -- Widgt    - Ttk_CheckButton which will be created
-   -- pathName - Tk path (starts with dot) for the widget
-   -- options  - Options which will be passed to the widget. Default value is
-      --            empty
-   -- Interp   - Tcl interpreter to which the widget will be created. If null,
-      --            the widget will be created in the "contextual" interpreter.
-      --            Default value is null.
-      -- SOURCE
+     -- ****f* TtkCheckButton/Create (procedure)
+     -- FUNCTION
+     -- Creates a new Ttk_CheckButton in the specified interpreter.
+     -- PARAMETERS
+     -- Widgt    - Ttk_CheckButton which will be created
+     -- pathName - Tk path (starts with dot) for the widget
+     -- options  - Options which will be passed to the widget. Default value is
+     --            empty
+     -- Interp   - Tcl interpreter to which the widget will be created. If null,
+     --            the widget will be created in the "contextual" interpreter.
+     --            Default value is null.
+     -- HISTORY
+     -- 8.6.1 - Added
+     -- TODO
+     -- Replace it with higher level of binding
+     -- SOURCE
    overriding procedure Create
      (Widgt: out Ttk_CheckButton; pathName: in String;
       options: in String := ""; Interp: in Tcl_Interp := null) with
