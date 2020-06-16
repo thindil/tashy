f255da320d9baf151138a15f6ca187ea162f899f
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-06-16 07:31:48 +0000

    updated code documentation
    FossilOrigin-Name: 98ac6687df96e8f1a5d84b37bc5051dadc8977754051480c5f262c3ae700c62e

--- src/tk/tcl-tk-ada-widgets-ttkbutton-ttkradiobutton.ads
+++ src/tk/tcl-tk-ada-widgets-ttkbutton-ttkradiobutton.ads
@@ -48,25 +48,33 @@ package Tcl.Tk.Ada.Widgets.TtkButton.TtkRadioButton is
    --            Default value is null.
    -- RESULT
    -- Newly created Ttk_RadioButton
+   -- HISTORY
+   -- 8.6.1 - Added
+   -- TODO
+   -- Replace it with higher level of binding
    -- SOURCE
    overriding function Create
      (pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) return Ttk_RadioButton with
       Pre => pathName /= "";
-      -- ****
+     -- ****
 
-   -- ****f* TtkRadioButton/Create (procedure)
-   -- FUNCTION
-   -- Creates a new Ttk_RadioButton in the specified interpreter.
-   -- PARAMETERS
-   -- Widgt    - Ttk_RadioButton which will be created
-   -- pathName - Tk path (starts with dot) for the widget
-   -- options  - Options which will be passed to the widget. Default value is
-      --            empty
-   -- Interp   - Tcl interpreter to which the widget will be created. If null,
-      --            the widget will be created in the "contextual" interpreter.
-      --            Default value is null.
-      -- SOURCE
+     -- ****f* TtkRadioButton/Create (procedure)
+     -- FUNCTION
+     -- Creates a new Ttk_RadioButton in the specified interpreter.
+     -- PARAMETERS
+     -- Widgt    - Ttk_RadioButton which will be created
+     -- pathName - Tk path (starts with dot) for the widget
+     -- options  - Options which will be passed to the widget. Default value is
+     --            empty
+     -- Interp   - Tcl interpreter to which the widget will be created. If null,
+     --            the widget will be created in the "contextual" interpreter.
+     --            Default value is null.
+     -- HISTORY
+     -- 8.6.1 - Added
+     -- TODO
+     -- Replace it with higher level of binding
+     -- SOURCE
    overriding procedure Create
      (Widgt: out Ttk_RadioButton; pathName: in String;
       options: in String := ""; Interp: in Tcl_Interp := null) with
