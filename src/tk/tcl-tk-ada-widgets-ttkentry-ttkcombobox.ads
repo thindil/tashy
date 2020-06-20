0790901074b7d6f2032203b0294d1a2e264af515
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-06-20 07:26:51 +0000

    updated code documentation
    FossilOrigin-Name: 2603804550f81ac823d65aee36f413d8e78ad62ae59534d7884bd2be6818923b

--- src/tk/tcl-tk-ada-widgets-ttkentry-ttkcombobox.ads
+++ src/tk/tcl-tk-ada-widgets-ttkentry-ttkcombobox.ads
@@ -55,6 +55,10 @@ package Tcl.Tk.Ada.Widgets.TtkEntry.TtkComboBox is
    --            Default value is null.
    -- RESULT
    -- Newly created Ttk_ComboBox
+   -- HISTORY
+   -- 8.6.2 - Added
+   -- TODO
+   -- Replace it with higher level of binding
    -- SOURCE
    function Create
      (pathName: in String; options: in String := "";
@@ -62,17 +66,21 @@ package Tcl.Tk.Ada.Widgets.TtkEntry.TtkComboBox is
       Pre => pathName /= "";
       -- ****
 
-   -- ****f* TtkComboBox/Create (procedure)
-   -- FUNCTION
-   -- Creates a new Ttk_ComboBox in the specified interpreter.
-   -- PARAMETERS
-   -- Widgt    - Ttk_ComboBox which will be created
-   -- pathName - Tk path (starts with dot) for the widget
-   -- options  - Options which will be passed to the widget. Default value is
+      -- ****f* TtkComboBox/Create (procedure)
+      -- FUNCTION
+      -- Creates a new Ttk_ComboBox in the specified interpreter.
+      -- PARAMETERS
+      -- Widgt    - Ttk_ComboBox which will be created
+      -- pathName - Tk path (starts with dot) for the widget
+      -- options  - Options which will be passed to the widget. Default value is
       --            empty
-   -- Interp   - Tcl interpreter to which the widget will be created. If null,
+      -- Interp   - Tcl interpreter to which the widget will be created. If null,
       --            the widget will be created in the "contextual" interpreter.
       --            Default value is null.
+      -- HISTORY
+      -- 8.6.2 - Added
+      -- TODO
+      -- Replace it with higher level of binding
       -- SOURCE
    procedure Create
      (Widgt: out Ttk_ComboBox; pathName: in String; options: in String := "";
