925753bd481b8fc71d696183d9ed98545d4d08cf
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-06-26 07:43:22 +0000

    added binding for command event add
    FossilOrigin-Name: e6b445775c46f6a82dc1e86e581b42f61b9fc1d82a6c3a5d0e5ddfdc3f79b0af

---
+++ src/tk/tcl-tk-ada-event.ads
@@ -0,0 +1,49 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- Tashy is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+-- ****h* TkAda/Event
+-- FUNCTION
+-- Provides code for manipulate Tk command Event
+-- SOURCE
+package Tcl.Tk.Ada.Event is
+-- ****
+
+   -- ****f* Event/Add
+   -- FUNCTION
+   -- Assiociated the selected virtual event with the selected sequence on the
+   -- selected interpreter.
+   -- PARAMETERS
+   -- Virtual  - Name of the virtual event which will be triggered when the
+   --            selected Sequence occurs
+   -- Sequence - Tcl commands which will trigger the selected Virtual event
+   -- Interp   - Tcl interpreter on which the event should be added
+   -- HISTORY
+   -- 8.6.6 - Added
+   -- TODO
+   -- Replace it with higher level of binding
+   -- SOURCE
+   procedure Add
+     (Virtual, Sequence: in String; Interp: Tcl_Interp := Get_Context) with
+      Pre => Virtual /= "" and Sequence /= "";
+      -- ****
+
+end Tcl.Tk.Ada.Event;
