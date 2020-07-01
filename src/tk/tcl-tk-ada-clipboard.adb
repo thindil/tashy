03de2dc521ba4a19129f4ee37e15302ac3f6356a
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-07-01 07:30:08 +0000

    added binding for command append
    FossilOrigin-Name: af03bbd4041f84e774112950e0318bf6fd33f318b1aeeb8a2bb5f737eaefc32e

---
+++ src/tk/tcl-tk-ada-clipboard.adb
@@ -0,0 +1,35 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- Tashy is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+package body Tcl.Tk.Ada.Clipboard is
+
+   procedure Append
+     (Data: in String; Window: in Tk_Widget'Class;
+      Format, CType: in String := "STRING") is
+   begin
+      Tcl_Eval
+        (Window.Interp,
+         "clipboard append -displayof " & Widget_Image(Window) & " -format " &
+         Format & " -type " & CType & " " & Data);
+   end Append;
+
+end Tcl.Tk.Ada.Clipboard;
---
+++ src/tk/tcl-tk-ada-clipboard.ads
@@ -0,0 +1,54 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- Tashy is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+with Tcl.Tk.Ada.Widgets; use Tcl.Tk.Ada.Widgets;
+
+-- ****h* TkAda/Clipboard
+-- FUNCTION
+-- Provides code for manipulate Tk clipboard and selection
+-- SOURCE
+package Tcl.Tk.Ada.Clipboard is
+-- ****
+
+   -- ****f* Clipboard/Append
+   -- FUNCTION
+   -- Add the selected data to the clipboard
+   -- PARAMETERS
+   -- Data   - The data which will be added to the clipboard
+   -- Window - Tk_Widget on which display the data will be added to clipboard
+   -- Format - Format in which the data is transmitted to the requester.
+   --          Possible values are STRING or ATOM. Default value is STRING
+   -- CType  - Type of desired selection return. Possible values are STRING or
+   --          FILE_NAME. Default value is STRING
+   -- HISTORY
+   -- 8.6.7 - Added
+   -- TODO
+   -- Replace it with higher level of binding
+   -- SOURCE
+   procedure Append
+     (Data: in String; Window: in Tk_Widget'Class;
+      Format, Ctype: in String := "STRING") with
+      Pre => Data /= "" and Format in "STRING" | "ATOM" and
+      Ctype in "STRING" | "FILE_NAME";
+      -- ****
+
+end Tcl.Tk.Ada.Clipboard;
