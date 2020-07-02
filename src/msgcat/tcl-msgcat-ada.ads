a607325a6b8076f72682176c63ee1c42f0b0638b
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-07-02 07:38:47 +0000

    started work on binding for msgcat package
    FossilOrigin-Name: 288b3c6ec9b263496d2f439fac7a33158d1eec1dfd6c0f1ba49863d6b9e10094

---
+++ src/msgcat/tcl-msgcat-ada.ads
@@ -0,0 +1,39 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- Tashy is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+-- ****h* MsgCat/MsgCatAda
+-- FUNCTION
+-- Dummy package
+-- SOURCE
+package Tcl.MsgCat.Ada is
+-- ****
+
+   -- ****f* MsgCatAda/MsgCat_Init
+   -- FUNCTION
+   -- Load package msgcat. Raise Program_Error if package can't be loaded
+   -- PARAMETERS
+   -- Interp: Tcl interpreter to which msgcat package will be added
+   -- SOURCE
+   procedure MsgCat_Init(Interp: in Tcl_Interp);
+   -- ****
+
+end Tcl.MsgCat.Ada;
