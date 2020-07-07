0f4453122ff0531bd7c1c2bb28810e28517bf127
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-07-07 07:30:50 +0000

    started work on autoscroll binding
    FossilOrigin-Name: 1eaec2c36fd1ff2d27506081b650315ba412b3bd0101373d9400d3f6cedbc325

---
+++ src/tklib/tcl-tklib-ada-autoscroll.adb
@@ -0,0 +1,36 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- Tashy is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+with Tcl.Ada;
+
+package body Tcl.Tklib.Ada.Autoscroll is
+
+   procedure Autoscroll_Init(Interp: in Tcl_Interp) is
+   begin
+      if Tcl.Ada.Tcl_Eval(Interp, "package require autoscroll") /= 0 then
+         raise Program_Error
+           with "Failed to load autoscroll package: " &
+           Tcl.Ada.Tcl_GetStringResult(Interp);
+      end if;
+   end Autoscroll_Init;
+
+end Tcl.Tklib.Ada.Autoscroll;
