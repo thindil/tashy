48f499e136335adf4c731dfe00bec42189ade2fb
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-07-09 07:27:48 +0000

    added binding for command add
    FossilOrigin-Name: 8aaf97dae7c7c0a113a8c6094955460d7730e86bc2823e7de50afd53122ef6e3

---
+++ src/tk/tcl-tk-ada-option.adb
@@ -0,0 +1,35 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- Tashy is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+with Tcl.Ada;
+
+package body Tcl.Tk.Ada.Option is
+
+   procedure Add
+     (Interp: in Tcl_Interp; Pattern, Value: in String;
+      OPriority: in String := "interactive") is
+   begin
+      Tcl.Ada.Tcl_Eval
+        (Interp, "option add " & Pattern & " " & Value & " " & OPriority);
+   end Add;
+
+end Tcl.Tk.Ada.Option;
---
+++ src/tk/tcl-tk-ada-option.ads
@@ -0,0 +1,50 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- Tashy is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+
+-- ****h* TkAda/Option
+-- FUNCTION
+-- Provides code for manipulate Tk command Option
+-- SOURCE
+package Tcl.Tk.Ada.Option is
+-- ****
+
+   -- ****f* Option/Add
+   -- FUNCTION
+   -- Add a new option to the database
+   -- PARAMETERS
+   -- Interp    - Tcl interpreter on which the new option will be added
+   -- Pattern   - The option which will be added
+   -- Value     - Value of the option to add
+   -- OPriority - Priority of the new option. Can be empty. Default value is
+   --             interactive
+   -- HISTORY
+   -- 8.6.7 - Added
+   -- TODO
+   -- Replace it with higher level of binding
+   -- SOURCE
+   procedure Add
+     (Interp: in Tcl_Interp; Pattern, Value: in String;
+      OPriority: in String := "interactive") with
+      Pre => Pattern /= "" and Value /= "";
+      -- ****
+
+end Tcl.Tk.Ada.Option;
