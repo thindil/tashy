f9615ed0819b7625bb3c98e76243dc030ced3cd4
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-08-03 06:02:13 +0000

    fixed code documentation
    FossilOrigin-Name: b6ff9f4498bf0a0b6cdf3aa3a12f316030209fcf3710681c791060cf48a32157

--- src/tk/tcl-tk-ada-widgets-ttkentry-ttkcombobox.ads
+++ src/tk/tcl-tk-ada-widgets-ttkentry-ttkcombobox.ads
@@ -27,7 +27,7 @@
 -- however invalidate any other reasons why the executable file might be
 -- covered by the GNU Public License.
 
--- ****h* Widgets/TtkComboBox
+-- ****h* TtkEntry/TtkComboBox
 -- FUNCTION
 -- Provides code for manipulate Tk widget ComboBox
 -- SOURCE
