6f691910dd438fd43948a4516b848aeb6eec5835
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-08-09 07:27:58 +0000

    updated code documentation
    FossilOrigin-Name: 4a20a7c9e6641fe82799070c48955ce9b11386b2ee4d8e605ef6ed300da16965

--- src/tk/tcl-tk-ada-widgets-button-radiobutton.ads
+++ src/tk/tcl-tk-ada-widgets-button-radiobutton.ads
@@ -34,11 +34,10 @@ package Tcl.Tk.Ada.Widgets.Button.RadioButton is
    type Tk_RadioButton is new Tk_Button with private;
    -- ****
 
-   -- ****f* RadioButton/Create
+   -- ****f* RadioButton/Create (function)
    -- FUNCTION
    -- Creates a new Tk_RadioButton in the specified interpreter.
    -- PARAMETERS
-   -- Widgt    - Tk_RadioButton which will be created
    -- pathName - Tk path (starts with dot) for the widget
    -- options  - Options which will be passed to the widget. Default value is
    --            empty
@@ -51,27 +50,70 @@ package Tcl.Tk.Ada.Widgets.Button.RadioButton is
    -- 8.6.1 - Moved from Tcl.Tk.Ada.Widgets
    -- TODO
    -- Replace it with higher level of binding
+   -- EXAMPLE
+   -- -- Create a new button with pathname .mybutton and label select me
+   -- My_Button: constant Tk_RadioButton := Create(".mybutton", "-text {select me}");
+   -- COMMANDS
+   -- radiobutton pathName ?options?
+   -- SEE ALSO
+   -- Create (procedure)
    -- SOURCE
    overriding function Create
      (pathName: in String; options: in String := "";
       Interp: in Tcl_Interp := null) return Tk_RadioButton with
       Pre => pathName /= "";
-   overriding procedure Create
-     (Widgt: out Tk_RadioButton; pathName: in String; options: in String := "";
-      Interp: in Tcl_Interp := null) with
-      Pre => pathName /= "";
-      -- ****
 
-      -- ****f* RadioButton/Deselect
-      -- FUNCTION
-      -- Deselect the button.
-      -- PARAMETERS
-      -- Buttn - Tk_RadioButton to deselect
+
+     -- ****f* RadioButton/Create (procedure)
+     -- FUNCTION
+     -- Creates a new Tk_RadioButton in the specified interpreter.
+     -- PARAMETERS
+     -- Widgt    - Tk_RadioButton which will be created
+     -- pathName - Tk path (starts with dot) for the widget
+     -- options  - Options which will be passed to the widget. Default value is
+     --            empty
+     -- Interp   - Tcl interpreter to which the widget will be created. If null,
+     --            the widget will be created in the "contextual" interpreter.
+     --            Default value is null.
+     -- OUTPUT
+     -- Newly created Tk_RadioButton as parameter Widgt
      -- HISTORY
      -- 8.6.1 - Moved from Tcl.Tk.Ada.Widgets
      -- TODO
      -- Replace it with higher level of binding
-      -- SOURCE
+     -- EXAMPLE
+     -- -- Create radiobutton My_Button with pathname .mybutton and label click me on the current Tcl interpreter
+     -- declare
+     --    My_Button: Tk_RadioButton;
+     -- begin
+     --    Create(My_Button, ".mybutton", "-text {click me}");
+     -- end;
+     -- COMMANDS
+     -- radiobutton pathName ?options?
+     -- SEE ALSO
+     -- Create (function)
+     -- SOURCE
+   overriding procedure Create
+     (Widgt: out Tk_RadioButton; pathName: in String; options: in String := "";
+      Interp: in Tcl_Interp := null) with
+      Pre => pathName /= "";
+     -- ****
+
+     -- ****f* RadioButton/Deselect
+     -- FUNCTION
+     -- Deselect the button.
+     -- PARAMETERS
+     -- Buttn - Tk_RadioButton to deselect
+     -- HISTORY
+     -- 8.6.1 - Moved from Tcl.Tk.Ada.Widgets
+     -- TODO
+     -- Replace it with higher level of binding
+     -- EXAMPLE
+     -- -- Deselect My_Radiobutton
+     -- Deselect(My_Radiobutton);
+     -- COMMANDS
+     -- Buttn deselect
+     -- SOURCE
    procedure Deselect(Buttn: in Tk_RadioButton);
    -- ****
 
@@ -84,6 +126,11 @@ package Tcl.Tk.Ada.Widgets.Button.RadioButton is
    -- 8.6.1 - Moved from Tcl.Tk.Ada.Widgets
    -- TODO
    -- Replace it with higher level of binding
+   -- EXAMPLE
+   -- -- Select My_Radiobutton
+   -- Tk_Select(My_Radiobutton);
+   -- COMMANDS
+   -- Buttn select
    -- SOURCE
    procedure Tk_Select(Buttn: in Tk_RadioButton);
    -- ****
