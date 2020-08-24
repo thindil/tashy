cf5c36df00d1a0a281c5c84c7064345034eabba4
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-08-24 05:57:58 +0000

    updated code documentation
    FossilOrigin-Name: 2b447ce1e2941b54524f2cdf25e5212dda361527179a5c84e2e7a4eb5bf2b87e

--- src/tk/tcl-tk-ada-widgets-ttkbutton-ttkcheckbutton.ads
+++ src/tk/tcl-tk-ada-widgets-ttkbutton-ttkcheckbutton.ads
@@ -52,6 +52,13 @@ package Tcl.Tk.Ada.Widgets.TtkButton.TtkCheckButton is
    -- 8.6.1 - Added
    -- TODO
    -- Replace it with higher level of binding
+   -- EXAMPLE
+   -- -- Create a new checkbutton with pathname .mybutton and text hello world
+   -- My_Button: constant Ttk_CheckButton := Create(".mybutton", "-text {hello world}");
+   -- COMMANDS
+   -- ttk::checkbutton pathName ?options?
+   -- SEE ALSO
+   -- Create (procedure)
    -- SOURCE
    overriding function Create
      (pathName: in String; options: in String := "";
@@ -74,6 +81,17 @@ package Tcl.Tk.Ada.Widgets.TtkButton.TtkCheckButton is
      -- 8.6.1 - Added
      -- TODO
      -- Replace it with higher level of binding
+     -- EXAMPLE
+     -- -- Create checkbutton My_Button with pathname .mybutton and selected state on the current Tcl interpreter
+     -- declare
+     --    My_Button: Ttk_CheckButton;
+     -- begin
+     --    Create(My_Button, ".mybutton", "-state selected");
+     -- end;
+     -- COMMANDS
+     -- ttk::checkbutton pathName ?options?
+     -- SEE ALSO
+     -- Create (function)
      -- SOURCE
    overriding procedure Create
      (Widgt: out Ttk_CheckButton; pathName: in String;
